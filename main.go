2bc8f3a15ebf9f2ab990bae3a2820098044d7ee8
Jss <275442646@qq.com>
2025-12-01 23:37:10 +0800

    作业1

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	task1 "job/task-1"
+)
+
+func main() {
+	fmt.Println(task1.TwoSum([]int{2, 7, 11, 15}, 17))
+
+	fmt.Println(task1.PlusOne([]int{9, 9, 7, 9}))
+}
--- task-1/task-1.go
+++ task-1/task-1.go
@@ -1,7 +1,36 @@
 package task1
 
-import "fmt"
+// 两数之和
+func TwoSum(nums []int, target int) []int {
+	mm := make(map[int]int)
+	for i := range nums {
+		need := target - nums[i]
+		if _, ok := mm[need]; ok {
+			return []int{mm[need], i}
+		}
+		mm[nums[i]] = i
+	}
+	return nil
+}
 
-func main() {
-	fmt.Println("Hello, World!")
+// 加一
+func PlusOne(digits []int) []int {
+	ll := len(digits)
+	digits[ll-1]++
+	if digits[ll-1] < 10 {
+		return digits
+	}
+	for i := ll - 1; i >= 0; i-- {
+		if digits[i] == 10 {
+			digits[i] = 0
+		} else {
+			break
+		}
+		if i == 0 {
+			digits = append([]int{1}, digits...)
+		} else {
+			digits[i-1]++
+		}
+	}
+	return digits
 }
